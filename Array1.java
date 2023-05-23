d44d70b4e4cffa0b79493c4ceb0cc29c75183add
Harshit Kumar <kumarharshit6206@gmail.com>
2023-05-23 18:13:55 +0530

    Array1

---
+++ Array1.java
@@ -0,0 +1,18 @@
+public class Array1 {
+    public static void main(String[] args) {
+        int arr[] = new int[7];
+        arr[0] = 4;
+        arr[1] = 5;
+        arr[2] = 6;
+        arr[3] = 7;
+        arr[4] = 4;
+        arr[5] = 8;
+        arr[6] = 9;
+
+        for (int i = 0; i <= 6; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
+
+
