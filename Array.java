287c2f33d1152298ea42c28b77114e270fb8ff10
Harshit Kumar <kumarharshit6206@gmail.com>
2023-05-23 17:58:14 +0530

    1D Array

---
+++ Array.java
@@ -0,0 +1,8 @@
+public class Array {
+    public static void main(String[] args) {
+        int arr[] = {1,3,5,7,9};
+        for(int num : arr){
+            System.out.println(num);
+        }
+    }
+}
