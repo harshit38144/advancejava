b350c7411b5a04d0b4e02e74e06aa26328bd2ba8
Harshit Kumar <kumarharshit6206@gmail.com>
2023-06-09 17:16:53 +0530

    KK Pattern9

---
+++ KkPattern9.java
@@ -0,0 +1,16 @@
+public class KkPattern9 {
+    public static void main(String[] args){
+        int n=9;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(j>=i && i<=(n-1)/2 && i+j<=n-1){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
