a31b4e1376e34b91cff02c139d393e3275b6d6c2
Harshit Kumar <kumarharshit6206@gmail.com>
2023-06-09 17:13:11 +0530

    KK Pattern6

---
+++ KkPattern6.java
@@ -0,0 +1,16 @@
+public class KkPattern6 {
+    public static void main(String[] args){
+        int n=8;
+        for(int i=1; i<n; i++){
+            for(int j=1; j<n; j++){
+                if(i+j<=n){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
