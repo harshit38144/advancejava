2bbc8994e94326e960d98605466639be24d1baeb
Harshit Kumar <kumarharshit6206@gmail.com>
2023-06-09 17:14:23 +0530

    KK Pattern

---
+++ KkPattern7.java
@@ -0,0 +1,16 @@
+public class KkPattern7 {
+    public static void main(String[] args){
+        int n=8;
+        for(int i=1; i<n; i++){
+            for(int j=1; j<n; j++){
+                if(i<=j){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
